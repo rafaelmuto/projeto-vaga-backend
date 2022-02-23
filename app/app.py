30ff11e2e645600e15772c5884db6998b96cc1ee
rafaelmuto <r.nagahama@gmail.com>
2022-02-23 02:22:29 -0300

    mysql seems to be working....

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restx import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
 
 
 
 app = Flask(__name__)
 api = Api(app, title='api_test', description='api description goes here...', version='0.0a')
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:123456@localhost:3306/app_db'
+db = SQLAlchemy(app)
 
 from Controllers.Test import *
 
