f849f95016eb177d13f79ac6e796ffdf2cbd0d56
rafaelmuto <r.nagahama@gmail.com>
2022-02-24 21:58:08 -0300

    formating

--- app/app.py
+++ app/app.py
@@ -4,8 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
-
-
 app = Flask(__name__)
 api = Api(app, title='ACMEVita API', description='api description goes here...', version='0.0a')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:123456@db:3306/app'
